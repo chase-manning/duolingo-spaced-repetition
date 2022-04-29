6e382a0ce8e76479ad29025af836bdfdbee429aa
chase-manning <chase@manning.dev>
2022-04-29 13:57:50 +0100

    :bug: stop adding button twice

--- background.js
+++ background.js
@@ -16,7 +16,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 const init = () => {
   // Add Button
+  if (document.getElementById("dsr-button")) return;
   const button = document.createElement("button");
+  button.id = "dsr-button";
   button.classList.add("dsr_button");
   const img = new Image();
   img.src = "https://i.imgur.com/vKVcjYZ.png";
@@ -26,6 +28,7 @@ const init = () => {
     startSr();
   });
   var element = document.querySelector("[title='Practice']");
+  if (!element) return;
   var parent = element.parentElement;
   parent.style.display = "flex";
   parent.appendChild(button);
